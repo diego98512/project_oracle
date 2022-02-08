8ae572c6033f00a738ebcec8351280ee1034a42d
Diego Florez <diegopflorez@outlook.com>
2022-02-08 14:24:04 -0500

    Auto stash before merge of "main" and "origin/main"

---
+++ cmake-build-debug/CMakeFiles/project_oracle.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/diego/Game Dev/Projects/project_oracle/main.cpp" "/home/diego/Game Dev/Projects/project_oracle/cmake-build-debug/CMakeFiles/project_oracle.dir/main.cpp.o"
+  "/home/diego/Game Dev/Projects/project_oracle/src/GameManager.cpp" "/home/diego/Game Dev/Projects/project_oracle/cmake-build-debug/CMakeFiles/project_oracle.dir/src/GameManager.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
