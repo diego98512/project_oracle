2d99d5730ca4a97680171c266830639d36be93f5
Diego Florez <diegopflorez@outlook.com>
2022-02-10 07:48:23 -0500

    Add tilemaps and tilesets.

---
+++ graphics/tilesets/oos2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.0" name="oos2" tilewidth="16" tileheight="16" tilecount="8265" columns="57">
+ <image source="../Zelda Sprites/horon village.png" width="925" height="2321"/>
+</tileset>
